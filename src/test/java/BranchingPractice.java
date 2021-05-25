233f9373514a6a5e340cead7a5642f9729449f5c
Tuerxun Mikaaili <mikailtursun@gmail.com>
2021-05-25 16:24:32 -0700

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -2,5 +2,11 @@ public class BranchingPractice {
 
     public static void main(String[] args) {
 
+        // created new project
+        // enabled version control
+        // added .gitignore with .idea , target folder amd .iml file
+        // made initial commit by keeping track of all files
+        // shared project on github
+
     }
 }
